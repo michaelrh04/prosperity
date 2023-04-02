2a54536937540b80ba6e054b5e6d0181906ced82
Michael Hargreaves <contact@michaelrh04.co.uk>
2023-04-02 20:03:45 +0100

    Create interfaces

---
+++ Client/Models/IPurchasable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    /// <summary>
+    /// The interface for any purchasable/ownable tile. This covers all properties, stations, and utilities.
+    /// </summary>
+    internal interface IPurchasable : ILandable
+    {
+        #region Variables
+        // The owner of the property.
+        Player Owner { get; set; }
+        // Returns the amount of rent due when requested.
+        int Rent { get; }
+        // The mortgage valuation of the property (i.e., the amount returned when mortgaged).
+        int MortgageValuation { get; }
+        #endregion
+    }
+}
