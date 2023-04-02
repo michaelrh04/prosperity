e33890627f9fd10ebd3273ade8b3077b1362871d
Michael Hargreaves <contact@michaelrh04.co.uk>
2023-04-02 22:20:38 +0100

    Create game view-model

---
+++ Client/ViewModels/GameViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    /// <summary>
+    /// The ViewModel for the GameWindow.xaml. Handles game data and operation.
+    /// </summary>
+    internal class GameViewModel : INotifyPropertyChanged
+    {
+        #region Public variables
+        public ILandable[] Board { get; }
+        #endregion
+
+        #region INotifyPropertyChanged implementation
+        /// <summary>
+        /// Property changed event handler.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
+    }
+}
