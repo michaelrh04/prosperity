34fb3b48f7aed9c103b0a911d2707368de0fb72e
Michael Hargreaves <contact@michaelrh04.co.uk>
2023-04-02 18:02:44 +0100

    Rename main (initialisation) window

--- Client/MenuWindow.xaml.cs
+++ Client/MenuWindow.xaml.cs
@@ -16,11 +16,11 @@ using System.Windows.Shapes;
 namespace Client
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// The interaction logic for MenuWindow.xaml - the initialising window & the application's main menu.
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MenuWindow : Window
     {
-        public MainWindow()
+        public MenuWindow()
         {
             InitializeComponent();
         }
