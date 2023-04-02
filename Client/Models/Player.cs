64d41cfedb3fc6a2d626599e494e1dec58561fdd
Michael Hargreaves <contact@michaelrh04.co.uk>
2023-04-02 20:03:53 +0100

    Create player class

---
+++ Client/Models/Player.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    internal class Player : INotifyPropertyChanged
+    {
+        #region Variables
+        /// <summary>
+        /// The name of the player.
+        /// </summary>
+        public string Name { get; set; }
+        #endregion
+
+        #region INotifyPropertyChanged implementation
+        /// <summary>
+        /// Property changed event handler.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
+    }
+}
