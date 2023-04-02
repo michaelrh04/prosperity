94ef4acd4595242a8d233618d38f02061cc4a003
Michael Hargreaves <contact@michaelrh04.co.uk>
2023-04-02 22:12:01 +0100

    Amend player class to include jail counter

--- Client/Models/Player.cs
+++ Client/Models/Player.cs
@@ -14,6 +14,11 @@ namespace Client
         /// The name of the player.
         /// </summary>
         public string Name { get; set; }
+        /// <summary>
+        /// <para>The number of attempts at rolling a double that a player has left before they must pay a fee to exit jail.</para>
+        /// <para>This variable also serves as an indicator of whether or not a player is jailed (-1 = not jailed).</para>
+        /// </summary>
+        public int JailCounter { get; set; }
         #endregion
 
         #region INotifyPropertyChanged implementation
